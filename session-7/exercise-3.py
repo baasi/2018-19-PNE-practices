34cd68ca281505476b317e8a9b64c76db3f7ae9d
baasi <basijr10@gmail.com>
2019-02-12 16:57:07 +0100

    session 7

---
+++ session-7/exercise-3.py
@@ -0,0 +1,24 @@
+# We are programing our first client
+
+import socket
+
+# We create a socket for communicating with the server
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+print("Socket created")
+
+PORT = 8080
+IP = "212.128.253.64"
+
+while True:
+    file = input("Type a message: ")
+# Connect to the server
+    s.connect((IP ,PORT))
+
+    s.send(str.encode(file))
+
+    msg = s.recv(2048).decode("utf-8")
+    print("MESSAGE FROM SERVER")
+    print(msg)
+
+    print("the end")
