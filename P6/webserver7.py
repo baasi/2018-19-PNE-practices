61b865b760012a20b4c99b41544edd4efc572c7c
baasi <basijr10@gmail.com>
2019-05-03 13:17:40 +0200

    finished

--- P6/webserver7.py
+++ P6/webserver7.py
@@ -12,28 +12,29 @@ class TestHandler(http.server.BaseHTTPRequestHandler):
 
         global seq, op
         termcolor.cprint(self.requestline, 'green')
-        path = self.path
 
         if self.path == '/':
             with open("main.html", "r") as f:
-                contents = f.read()
+                info = f.read()
             resp = 200
-        elif path[:4] == '/get':
-            resp = 200
-            msg = path.split('&')
-            my_seq = msg[0].split('=')[1]
-            my_seq = my_seq.upper()
-            if my_seq.upper().strip('ACGT') == '':
-                my_seq = Seq(my_seq)
 
-                counting = {'base=A': ('Count A: ' + str(my_seq.counting('A'))),
-                         'base=C': ('Count C: ' + str(my_seq.counting('C'))),
-                         'base=G': ('Count G: ' + str(my_seq.counting('G'))),
-                         'base=T': ('Count T: ' + str(my_seq.counting('T')))}
-                percentage = {'base=A': ('Percentage A: ' + str(my_seq.percentage('A')) + '%'),
-                        'base=C': ('Percentage C: ' + str(my_seq.percentage('C')) + '%'),
-                        'base=T': ('Percentage T: ' + str(my_seq.percentage('T')) + '%'),
-                        'base=G': ('Percentage G: ' + str(my_seq.percentage('G')) + '%')}
+        elif self.path[:4] == '/get':
+            resp = 200
+            msg = self.path.split('&')
+            seqq = msg[0].split('=')[1]
+            seqq = seqq.upper()
+            if seqq.upper().strip('ACGT') == '':
+
+                seqq = Seq(seqq)
+
+                counting = {'base=A': ('Count A: ' + str(seqq.counting('A'))),
+                         'base=C': ('Count C: ' + str(seqq.counting('C'))),
+                         'base=G': ('Count G: ' + str(seqq.counting('G'))),
+                         'base=T': ('Count T: ' + str(seqq.counting('T')))}
+                percentage = {'base=A': ('Percentage A: ' + str(seqq.percentage('A')) + '%'),
+                        'base=C': ('Percentage C: ' + str(seqq.percentage('C')) + '%'),
+                        'base=T': ('Percentage T: ' + str(seqq.percentage('T')) + '%'),
+                        'base=G': ('Percentage G: ' + str(seqq.percentage('G')) + '%')}
 
                 result = {'count': counting, 'perc': percentage}
 
@@ -46,7 +47,7 @@ class TestHandler(http.server.BaseHTTPRequestHandler):
                         op = result[operation][bases]
 
                 elif len(msg) == 4:
-                    seq = 'Length: ' + str(my_seq.len())
+                    seq = 'Length: ' + str(seqq.len())
                     operation = msg[3].split('=')[1]
                     bases = msg[2]
 
@@ -56,29 +57,29 @@ class TestHandler(http.server.BaseHTTPRequestHandler):
                 with open('response.html', 'w') as f:
                     data = """<!DOCTYPE html><html lang="en"><head><meta charset="UTF-8"><title>RESPONSE</title></head><body><h1>results page:</h1><p>{}</p><p>len</p><p>op</p><a href="/">[Main Page]</a></body></html>"""
                     print(op)
-                    data = data.replace("{}", my_seq.strbases.upper()).replace("len", seq).replace("op", op)
+                    data = data.replace("{}", seqq.strbases.upper()).replace("len", seq).replace("op", op)
                     f.write(data)
                     f.close()
                     with open('response.html', 'r') as f:
-                        contents = f.read()
+                        info = f.read()
             else:
                 with open('sequenceerror.html', 'r') as f:
-                    contents = f.read()
+                    info = f.read()
 
         else:
             resp = 404
             with open("error.html", "r") as f:
-                contents = f.read()
+                info = f.read()
 
         self.send_response(resp)
 
         self.send_header('Content-Type', 'text/html')
 
-        self.send_header('Content-Length', len(str.encode(contents)))
+        self.send_header('Content-Length', len(str.encode(info)))
 
         self.end_headers()
 
-        self.wfile.write(str.encode(contents))
+        self.wfile.write(str.encode(info))
         return
 
 
